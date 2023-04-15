b57b137d89cd12972652f9901ad81a458a1d62b8
masoris <masoris@gmail.com>
2023-04-15 17:23:05 +0900

    3

--- memlingo.py
+++ memlingo.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory, render_template, request, make_response, jsonify
+import subprocess, os
 
 app = Flask(__name__)
 
@@ -46,13 +47,46 @@ def speak():
 	voicename = request.json['voicename']
 	textdata = request.json['textdata']
 
+	old_dir = os.curdir
+	os.chdir("../tts")
+	if lang == "eo":
+		cmd = ["python3", "esp_polish_transcription.py", "-e" ,"%s" % textdata, "%s" % voicename]
+	else:
+		cmd = ["python3", "esp_polish_transcription.py", "-p" ,"%s" % textdata, "%s" % voicename]
+	# print(cmd)
+	output = subprocess.run(cmd, stdout=subprocess.PIPE) 
+	os.chdir(old_dir)
+	# print(output.stdout.decode('utf-8'))
 
-	        # jsondata = {lang: field, voicename: voice, textdata: text};
-
-	result = {'resp': 'OK', 'lang':lang, 'voicename':voicename, 'textdata':textdata}
+	lines = output.stdout.decode('utf-8').split('\n')
+	print (lines)
+	for line in lines:
+		if line.find("pol_txt=") == 0:
+			pol_txt = line[8:]
+		if line.find("esp_txt=") == 0:
+			esp_txt = line[8:]
+	result = {'resp': 'OK', 'lang':lang, 'voicename':voicename, 'textdata':textdata, 'pol_txt':pol_txt}
 	resp = make_response(jsonify(result))
 	return resp
-		
+
+@app.route('/tts/remember.api', methods=['POST'])
+def remember():
+	eo_txt = request.json['eo_txt']
+	pl_txt = request.json['pl_txt']
+	voice_name = request.json['voice_name']
+
+	old_dir = os.curdir
+	os.chdir("../tts")
+	
+	cmd = ["python3", "remember.py","%s" % eo_txt, "%s" % pl_txt, "%s" % voice_name]
+	
+	output = subprocess.run(cmd, stdout=subprocess.PIPE) 
+	os.chdir(old_dir)
+	print(output.stdout.decode('utf-8'))
+	result = {'resp': 'OK'}
+	resp = make_response(jsonify(result))
+	return resp
+				
 app.run(debug=True, host='192.168.117.129', port=5000)
 
 
