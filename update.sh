ec448bdb65c3a7c890d682dece2f898225f18e64
masoris <masoris@gmail.com>
2023-07-07 17:11:44 +0900

    0707

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ssh memlingo@141.164.48.63 "cd ~/svc/memlingo/ && ./stop.sh"
-rsync -avz -e 'ssh -p 22' ./pages/ memlingo@141.164.48.63:~/svc/memlingo/pages/
-rsync -avz -e 'ssh -p 22' ./sounds/ memlingo@141.164.48.63:~/svc/memlingo/sounds/
-rsync -avz -e 'ssh -p 22' ./memlingo.py memlingo@141.164.48.63:~/svc/memlingo/memlingo.py
+rsync --delete -avz -e 'ssh -p 22' ./pages/ memlingo@141.164.48.63:~/svc/memlingo/pages/
+rsync --delete -avz -e 'ssh -p 22' ./sounds/ memlingo@141.164.48.63:~/svc/memlingo/sounds/
+rsync --delete -avz -e 'ssh -p 22' ./memlingo.py memlingo@141.164.48.63:~/svc/memlingo/memlingo.py
 #scp -r ./pages/* memlingo@141.164.48.63:~/svc/memlingo/pages/
 #scp -r ./sounds/* memlingo@141.164.48.63:~/svc/memlingo/sounds/
 #scp -r ./memlingo.py memlingo@141.164.48.63:~/svc/memlingo/
